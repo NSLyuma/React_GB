e05ebc0392a0935106ecc1888e9bc74052d66a56
NSLyuma <iinokayo@mail.ru>
2022-02-19 19:49:41 +0300

    Started to do list

--- react-base/src/App.js
+++ react-base/src/App.js
@@ -1,12 +1,61 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.sass";
 import { Message } from "./components";
+import {
+  FILTER_BY_STATUS_ALL,
+  FILTER_BY_STATUS_COMPLETED,
+  FILTER_BY_STATUS_IN_WORK,
+} from "./constants";
+import { useCreateTaskForm } from "./hooks/useCreateTaskForm";
+import { useFilterByStatus } from "./hooks/useFilterByStatus";
+import { useTaskFilteredByStatus } from "./hooks/useTaskFilteredByStatus";
+import { useTaskList } from "./hooks/useTaskList";
 
 function App() {
-  const text = "Hello, world!";
+  const { taskList, addNewTask, changeStatus } = useTaskList();
+
+  useEffect(() => {
+    localStorage.setItem("task-list", JSON.stringify(taskList));
+  }, [taskList]);
+
+  const { filterStatus, onChangeStatus } = useFilterByStatus();
+
+  const { filteredList } = useTaskFilteredByStatus({
+    list: taskList,
+    filterStatus,
+  });
+
+  const { handleSubmit, onChangeInput, inputValue } = useCreateTaskForm({
+    onSubmit: addNewTask,
+  });
+
   return (
     <div>
-      <Message message={text} />
+      <form onSubmit={handleSubmit}>
+        <input onChange={onChangeInput} value={inputValue} type="text" />
+        <button type="submit">Save</button>
+      </form>
+
+      <select value={filterStatus} onChange={onChangeStatus}>
+        <option value={FILTER_BY_STATUS_ALL}>All</option>
+        <option value={FILTER_BY_STATUS_IN_WORK}>In work</option>
+        <option value={FILTER_BY_STATUS_COMPLETED}>Completed</option>
+      </select>
+
+      <ul>
+        {filteredList.map(({ status, text }, index) => {
+          return (
+            <li key={index}>
+              <input
+                onChange={changeStatus(index, status)}
+                checked={status}
+                type="checkbox"
+              />
+              {text}
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 }
---
+++ react-base/src/hooks/useTaskList.js
@@ -0,0 +1,25 @@
+import { useCallback, useState } from "react";
+
+export const useTaskList = () => {
+  const [taskList, setTaskList] = useState([]);
+
+  const addNewTask = useCallback((text) => {
+    const task = { status: false, text };
+    setTaskList((prevState) => {
+      return [...prevState, task];
+    });
+  }, []);
+
+  const changeStatus = useCallback(
+    (index, status) => () => {
+      setTaskList((prevTaskList) => {
+        const cloneTaskList = [...prevTaskList];
+        cloneTaskList[index] = { ...cloneTaskList[index], status: !status };
+        return cloneTaskList;
+      });
+    },
+    []
+  );
+
+  return { taskList, addNewTask, changeStatus };
+};
